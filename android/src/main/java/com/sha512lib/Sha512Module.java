4caea8e5030f9b5e6480621f6ecc82940040b1fc
Marc Lammers <lammers.marc@gmail.com>
2019-09-06 15:44:23 +0200

    Init StringBuilder.

--- android/src/main/java/com/sha512lib/Sha512Module.java
+++ android/src/main/java/com/sha512lib/Sha512Module.java
@@ -49,6 +49,7 @@ public class Sha512Module extends ReactContextBaseJavaModule {
             md = MessageDigest.getInstance("SHA-512");
             md.update(toHash.getBytes("UTF-8"));
             byte[] digest = md.digest();
+            StringBuilder sb = new StringBuilder();
             for(int i=0; i< bytes.length ;i++){
                 sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
             }
