cc7f6c37e2a5d915c3b16023cc79f8be3d38c372
Marc Lammers <lammers.marc@gmail.com>
2019-09-06 15:46:43 +0200

    Init byte array.

--- android/src/main/java/com/sha512lib/Sha512Module.java
+++ android/src/main/java/com/sha512lib/Sha512Module.java
@@ -50,8 +50,8 @@ public class Sha512Module extends ReactContextBaseJavaModule {
             md.update(toHash.getBytes("UTF-8"));
             byte[] digest = md.digest();
             StringBuilder sb = new StringBuilder();
-            for(int i=0; i< bytes.length ;i++){
-                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            for(int i=0; i< digest.length ;i++){
+                sb.append(Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1));
             }
             String hash = sb.toString();
             promise.resolve(hash);
